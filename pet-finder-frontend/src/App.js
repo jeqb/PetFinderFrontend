749c4c37ac2fb2cb5cef6081e9a84518fc8d4cb4
James <jameseqbonner@protonmail.com>
2021-08-04 11:40:12 -0700

    simple header added.

--- pet-finder-frontend/src/App.js
+++ pet-finder-frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, withRouter, Switch } from 'react-router-dom';
 
-function App() {
+// Components
+import Header from './components/layout/Header';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header/>
+      <div>Hello World</div>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default withRouter(App);
--- pet-finder-frontend/src/index.js
+++ pet-finder-frontend/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export const history = createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={history}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
